d87de961e998f61e1b84567098e71c7f4eaac4b9
Ming <kiming.wong@student.uts.edu.au>
2020-10-17 16:34:02 +0800

    1. Remove space in column names of facilites.csv 2. Print plots of suicide rate vs facilities for analysis

--- etl/data_cleaning.py
+++ etl/data_cleaning.py
@@ -3,17 +3,29 @@
 import os
 import pandas as pd
 
+# ========= Clean crude_suicide_rates Data =========
 # getting crude_suicide_rates Data
 suicide_rates_data_filepath = os.path.join("raw_data", "crude_suicide_rates.csv")
 suicide_rates_dataframe = pd.read_csv(suicide_rates_data_filepath)
 
 # strip the column names
-suicide_rates_dataframe = suicide_rates_dataframe.rename(columns = lambda x: x.strip())
+processed_suicide_rates_dataframe = suicide_rates_dataframe.rename(columns = lambda x: x.strip())
 
 # strip the dataframe
-suicide_rates_dataframe["Country"] = suicide_rates_dataframe["Country"].str.strip()
-suicide_rates_dataframe["Sex"] = suicide_rates_dataframe["Sex"].str.strip()
+processed_suicide_rates_dataframe["Country"] = processed_suicide_rates_dataframe["Country"].str.strip()
+processed_suicide_rates_dataframe["Sex"] = processed_suicide_rates_dataframe["Sex"].str.strip()
 
 # save the processed data to csv file
 processed_suicide_rates_data_filepath = os.path.join("processed_data", "crude_suicide_rates.csv")
-suicide_rates_dataframe.to_csv(processed_suicide_rates_data_filepath)
+processed_suicide_rates_dataframe.to_csv(processed_suicide_rates_data_filepath)
+
+# ========= Clean crude_suicide_rates Data =========
+country_facilities_data_filepath = os.path.join("raw_data", "facilities.csv")
+country_facilities_dataframe = pd.read_csv(country_facilities_data_filepath)
+
+# remove space in the column names
+processed_country_facilities_dataframe = country_facilities_dataframe.rename(columns = lambda x: x.replace(" ", ""))
+
+# save the processed data to csv file
+processed_country_facilities_data_filepath = os.path.join("processed_data", "facilities.csv")
+processed_country_facilities_dataframe.to_csv(processed_country_facilities_data_filepath)
